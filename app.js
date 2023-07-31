136051f31e85a41d8f13b4f6d1af2eed701d49d2
João Ramos <Joao.silvaramos2013@hotmail.com>
2023-07-31 12:47:41 -0300

    Arquivos bases configurados

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const registerRouter = require("./controllers/register");
+
+// Middleware for import css
+app.use(express.static("public"));
+// Middleware for routers
+app.use("/", registerRouter);
+
+
+app.listen(port, () => console.log(`Servidor rodando na porta: ${port}`));
