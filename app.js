4b59af5d1433b6c6290973560a8f962d3358788e
João Ramos <Joao.silvaramos2013@hotmail.com>
2023-07-31 21:43:22 -0300

    Finalizado o layout de home do projeto

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const routerHome = require("./controllers/home");
 
 // Middleware for import css
 app.use(express.static("public"));
+
+
+app.use("/", routerHome);
 
 app.listen(port, () => console.log(`Servidor rodando na porta: ${port}`));
