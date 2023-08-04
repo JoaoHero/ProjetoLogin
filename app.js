623f3acaaf0bb87fe8c819a0b4db2f21050e6419
João Ramos <Joao.silvaramos2013@hotmail.com>
2023-08-04 13:11:57 -0300

    Adicionado um middleware para remover barras no final da URL para evitar confuções no código, e problemas de seguranca

--- app.js
+++ app.js
@@ -14,6 +14,15 @@ app.use(cookieParser())
 app.use(express.urlencoded({
     extended: true
 }));
+// Middleware for remove the final bar, if exist
+app.use((req, res, next) => {
+    if (req.path.substr(-1) === '/' && req.path.length > 1) {
+      const query = req.url.slice(req.path.length);
+      res.redirect(301, req.path.slice(0, -1) + query);
+    } else {
+      next();
+    }
+});
 app.use(express.json());
 // Middleware for routers
 app.use("/", registerRouter);
